4da2385a1c3e0d32ff059242e642ac936414b2b8
TommyShaughnessy1 <tommy.shaughnessy@neueda.com>
2021-06-02 18:24:48 +0100

    Second_Commit_OOP_Applied

--- palindrome.py
+++ palindrome.py
@@ -1,18 +1,40 @@
-def palindrome_check():
+class Wordclass():
+    def __init__(self,name,word):
+        self.user_name = name
+        self.user_word = word
 
-    # gets user Input,strips white space and converts all letters to lower case
-    user_input = input("Enter a Word: ").strip().lower()
+    def palindrome_check(self):
 
-    #reverses the users input and gets the character at index[0]
-    reverse_user_input = user_input[::-1][0]
+        # gets user Input,strips white space and converts all letters to lower case
+        user_input = f"{self.user_word}".strip().lower()
 
-    # if user_input at index [0] = reverse_user_input[0]
-    if user_input[0] == reverse_user_input:
-        print(True)
-    else:
-        print(False)
+        #reverses the users input and gets the character at index[0]
+        reverse_user_input = user_input[::-1][0]
+
+        # if user_input at index [0] = reverse_user_input [0]
+
+        if user_input[0] == reverse_user_input:
+           return "True"
+        else:
+            return "False"
+
+while True:
+
+    get_name = input("Enter User Name: ")
+    get_word = input("Enter your Word: ")
+
+    new_class = Wordclass(get_name,get_word) 
+    print(f"Hi {new_class.user_name}! Your word is:{new_class.palindrome_check()}")
+    
+    
 
 
-palindrome_check()
+   
+    
+    
+
+
+
+
 
 
